dbc18fbcf8e06318b7438cea0256ced4e692c515
toinux <26522723+itzwam@users.noreply.github.com>
2023-05-14 17:06:40 +0100

    Update config.go - libpq handle env correctly, just passtrough with an "null" URLstring if url is none

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -863,10 +863,7 @@ func LoadConfig(file string) (*Config, error) {
 	parsedConfig.Section("postgres").MapTo(&psql)
 	if server.StoreBackend == "postgres" {
 		if psql.URL == "" {
-			psql.URL = os.Getenv("DATABASE_URL")
-			if psql.URL == "" {
-				return nil, ErrPostgresUnconfigured
-			}
+			psql.URL = 'postgres:///?sslmode=prefer'
 		}
 	}
 
